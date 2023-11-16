9b8f7332ec5509f885cb02aee0142fcb6850eb29
misrar09 <misrar09@gmail.com>
2023-11-16 17:29:20 +0100

    complete before  bonuses

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ const { createApp } = Vue;
 const vueApp = {
     data() {
         return {
-
+                    // slides array with objects
             slides: [
                 {
+                    id: 1,
                     image: 'https://img.freepik.com/free-vector/welcome-pattern-different-languages_23-2147870141.jpg',
                     title: 'Benvenuti nel nostro sito',
                     text: 'Esplora la nostra vasta gamma di prodotti e scopri offerte speciali.'
                 },
                 {
+                    id: 2,
                     image: 'https://img.freepik.com/free-vector/spring-sale-instagram-post_23-2148849921.jpg',
                     title: 'Nuova collezione primavera',
                     text: 'Scopri le ultime tendenze della moda con la nostra collezione primavera.'
                 },
                 {
+                    id: 3,
                     image: 'https://img.freepik.com/free-vector/special-offer-modern-sale-banner-template_1017-20667.jpg',
                     title: 'Offerte speciali',
                     text: 'Approfitta delle nostre offerte speciali. Sconti incredibili solo per te!'
@@ -27,15 +30,14 @@ const vueApp = {
             ],
 
 
-
-            activeImage: 0
+            activeImage: 0 // created slide counter
 
         }
 
     },
 
-    methods: {
-        btnDown() {
+    methods: { // methods with functions
+        btnDown() {  // down button function with condition to roll down the slides
 
             if (this.activeImage < this.slides.length - 1) {
                 this.activeImage++;
@@ -44,10 +46,9 @@ const vueApp = {
                 this.activeImage = 0;
             }
 
-
         },
 
-        btnUp() {
+        btnUp() { // up button function with condition to roll up the solde show
 
             if (this.activeImage > 0) {
                 this.activeImage--;
@@ -55,9 +56,6 @@ const vueApp = {
             else {
                 this.activeImage = this.slides.length - 1;
             }
-
-
-
 
         }
 
